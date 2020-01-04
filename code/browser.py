740eed503a5f3809d0c142eb97995b001977bf96
Roy <roy.healy87+gh@gmail.com>
2020-01-04 01:50:03 +0000

    Refactoring source text

--- code/browser.py
+++ code/browser.py
@@ -9,7 +9,8 @@ from PyQt5.QtGui import *
 from cssselect.xpath import ExpressionError
 from cssselect.parser import SelectorSyntaxError
 import traceback
-import html
+
+from text_processor import EnhancedTextViewer
 
 HOME = 'http://quotes.toscrape.com/'
 
@@ -28,7 +29,7 @@ class Main(QMainWindow):
         tabs = QTabWidget()
         self.browser = QtBrowser(main=self)
         self.queries = Queries(main=self)
-        self.source = SourceViewer()
+        self.source = EnhancedTextViewer()
         self.notes = QPlainTextEdit()
         tabs.addTab(self.browser, 'Browser')
         tabs.addTab(self.queries, 'Tools')
@@ -42,104 +43,6 @@ class Main(QMainWindow):
 
     def update_source(self, text):
         self.source.setPlainText(text)
-
-
-class SourceViewer(QWidget):
-    current = 0
-    total = 0
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.indexes = None
-        self.initUI()
-
-    def initUI(self):
-        grid = QGridLayout()
-        self.setLayout(grid)
-
-        search_button = QPushButton('Search')
-        grid.addWidget(search_button, 0, 1)
-        search_button.clicked.connect(self.find_pressed)
-
-        self.search_bar = QLineEdit()
-        grid.addWidget(self.search_bar, 0, 0)
-        self.search_bar.returnPressed.connect(self.find_pressed)
-
-        self.results = QLabel('0 of 0 Results')
-        grid.addWidget(self.results, 0, 2)
-
-        next_button = QPushButton('Next')
-        next_button.clicked.connect(self.next_pressed)
-        grid.addWidget(next_button, 0, 3)
-
-        previous_button = QPushButton('Previous')
-        previous_button.clicked.connect(self.previous_pressed)
-        grid.addWidget(previous_button, 0, 4)
-
-        self.source_text = QTextEdit()
-        grid.addWidget(self.source_text, 1, 0, 1, 5)
-        self.source_text.setReadOnly(True)
-
-        self.keywordFormat = QTextCharFormat()
-        self.keywordFormat.setBackground(Qt.yellow)
-        self.keywordFormat.setFontWeight(QFont.Bold)
-
-    def setPlainText(self, text):
-        self.source_text.setReadOnly(False)
-        self.source_text.setPlainText(text)
-        self.source_text.setReadOnly(True)
-
-    def find_pressed(self):
-        self.source_text.setReadOnly(False)
-        self.find_indexes()
-        self.current = 1
-        self.total = len(self.indexes)
-        self.set_format()
-        self.update_position()
-        self.source_text.setReadOnly(True)
-
-    def set_format(self):
-        for index in self.indexes:
-            cursor = self.source_text.textCursor()
-            size = len(self.search_bar.text())
-            cursor.setPosition(index)
-            cursor.setPosition(index + size, QTextCursor.KeepAnchor)
-            cursor.setCharFormat(self.keywordFormat)
-
-    def find_indexes(self):
-        search_term = self.search_bar.text().lower()
-        all_text = self.source_text.toPlainText().lower()
-        if not search_term or not all_text:
-            return
-        del self.indexes
-        self.indexes = [i for i in range(len(all_text)) if all_text.startswith(search_term, i)]
-
-    def next_pressed(self):
-        if not self.indexes:
-            return
-        self.current += 1
-        if self.current > self.total:
-            self.current = 1
-
-        self.update_position()
-
-    def previous_pressed(self):
-        if not self.indexes:
-            return
-        self.current -= 1
-        if self.current < 1:
-            self.current = self.total
-
-        self.update_position()
-
-    def update_position(self):
-        cursor = self.source_text.textCursor()
-        index = self.indexes[self.current - 1]
-        size = len(self.search_bar.text())
-        cursor.setPosition(index)
-        cursor.setPosition(index + size, QTextCursor.KeepAnchor)
-        self.source_text.setTextCursor(cursor)
-        self.results.setText(f'{self.current} of {self.total} Results')
 
 
 class QtBrowser(QWidget):
